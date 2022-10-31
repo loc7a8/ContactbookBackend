7d489ead65f8717586d431b736b449bf3e452079
loc7a8 <locb1910664@student.ctu.edu.vn>
2022-10-31 08:04:17 +0700

    Implement handlers

--- src/controllers/contact.controller.js
+++ src/controllers/contact.controller.js
@@ -27,26 +27,26 @@ exports.findAllFavorite = (req, res) => {
 };
 
 
-// const ContactService = require('../services/contact.service');
-// const ApiError = require('../api-error');
+const ContactService = require('../services/contact.service');
+const ApiError = require('../api-error');
 
-// exports.create = async (req, res, next) => {
-//     if (!req.body?.name){
-//         return next(new ApiError(400, 'Name can not be empty'));
+exports.create = async (req, res, next) => {
+    if (!req.body?.name){
+        return next(new ApiError(400, 'Name can not be empty'));
 
-//     }
-//     try {
-//         const contactService = new ContactService();
-//         const contact = await contactService.create(req.body);
-//         return res.send(contact);
-//     }
-//     catch (error) {
-//         console.log(error);
-//         return next(
-//             new ApiError(500, 'An error occured while creating the contact')
-//         );
-//     }
-// }
+    }
+    try {
+        const contactService = new ContactService();
+        const contact = await contactService.create(req.body);
+        return res.send(contact);
+    }
+    catch (error) {
+        console.log(error);
+        return next(
+            new ApiError(500, 'An error occured while creating the contact')
+        );
+    }
+}
 
 
 exports.findAll = async (req, res, next) => {
@@ -92,3 +92,81 @@ exports.findOne = async (req, res, next) => {
 };
 
 
+
+// Update a contact by the id in the request 
+exports.update = async (req, res, next) => {
+    if (Object.keys(req.body).length = 0) { 
+        return next(new ApiError (400, 'Data to update can not be empty'));
+}
+    
+    try {
+        const contactService = new ContactService();
+        const updated = await contactService.update(req.params.id, req.body);
+        if (!updated) {
+            return next(new ApiError (404, 'Contact not found'));
+        }
+        return res.send({ message: 'Contact was updated successfully' });
+    } catch (error) { 
+        console.log(error);
+        return next(
+            new ApiError(500, `Error updating contact with id-${req.params.id}`)
+        );
+    }
+
+// Delete a contact with the specified id in the request 
+exports.delete = async (req, res, next) => {
+    try {
+        const contactService = new ContactService();
+        const deleted = await contactService.delete(req.params.id); 
+        if (!deleted) {
+            return next(new ApiError (404, 'Contact not found'));
+        }
+        return res.send({ message: 'Contact was deleted successfully' });
+        } catch (error) {
+            console.log(error);
+            return next(
+                new ApiError(
+                    500,
+                    `Could not delete contact with id=${req.params.id}`
+                )
+            );
+        }
+    };
+
+
+// Find all favorite contacts of a user
+exports.findAllFavorite = async (req, res, next) => {
+    try {
+        const contactService = new ContactService(); 
+        const contacts = await contactService.allFavorite(); 
+        return res.send(contacts);
+    } catch (error) {
+    console.log(error);
+        return next(
+            new ApiError(
+                500,
+                'An error occurred while retrieving favorite contacts'
+            )
+        );
+    }
+};
+    
+
+
+// Delete all contacts of a user from the database 
+exports.deleteAll = async (req, res, next) => {
+    try {
+        const contactService = new ContactService();
+        const deleted = await contactService.deleteALL();
+        return res.send({
+            message: `${deleted} contacts were deleted successfully`,
+        });
+    } catch (error) {
+        console.log(error);
+        return next(
+            new ApiError (500, 'An error occurred while removing all contacts')
+        );
+    }
+};
+
+};
